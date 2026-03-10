653ae6a1dfac25fb7bb5ad7296aaf190f15939bb
Hirokazu Takatama <takatama@users.noreply.github.com>
2026-03-10 23:08:03 +0900

    Set PWA icon purpose to any maskable

--- vite.config.ts
+++ vite.config.ts
@@ -21,11 +21,13 @@ export default defineConfig({
             src: '/assets/images/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable',
           },
           {
             src: '/assets/images/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
